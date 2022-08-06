32ff92276eb13a8d1e3c9dcb5b1f849e134f5ee5
Fayouz <109908423+Fayouzz@users.noreply.github.com>
2022-08-07 02:30:47 +0530

    Update MountainArray.java

--- Solutions/MountainArray.java
+++ Solutions/MountainArray.java
@@ -1,6 +1,5 @@
 package com.fayouz;
 //https://leetcode.com/problems/peak-index-in-a-mountain-array/
-// https://leetcode.com/problems/find-peak-element/
 public class MountainArray {
     public static void main(String[] args) {
         int[] arr = {0, 1,0};
@@ -22,11 +21,6 @@ public class MountainArray {
                     start = mid + 1; // Because we know that mid+1 element > mid element
                 }
             }
-            // in the end, start == end and pointing to the largest number because of the 2 checks above
-            // start and end are always trying to find max element in the above 2 checks
-            // hence, when they are pointing to just one element, that is the max one because that is what the checks say
-            // more elaboration: at every point of time for start and end, they have the best possible answer till that time
-            // and if we are saying that only one item is remaining, hence cuz of above line that is the best possible ans
-            return start; // or return end as both are =
+            return start;
         }
     }
