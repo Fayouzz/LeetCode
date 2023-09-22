80c5fc93c33888800c950c6890f65e332cf87c2b
Fayouz <chappufayouz@gmail.com>
2023-09-23 00:16:24 +0530

    add file

---
+++ Solutions/1502. Can Make Arithmetic Progression From Sequence.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
+        int diff = arr[1]-arr[0];
+        for (int i = 1; i < arr.length; i++){
+            int val = arr[i]-diff;
+            if(val != arr[i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
