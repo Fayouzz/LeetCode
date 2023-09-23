a226d41a8a6273859a0875dd0bc4d84ad84dcd2c
Fayouz <chappufayouz@gmail.com>
2023-09-24 01:21:06 +0530

    add files

---
+++ Solutions/441. Arranging Coins.java
@@ -0,0 +1,9 @@
+//https://leetcode.com/problems/arranging-coins/
+
+class Solution {
+    public int arrangeCoins(int n) {
+        long i=0;
+        for(i=1;i*(i+1)/2<=n;i++);
+        return (int)i-1;
+    }
+}
